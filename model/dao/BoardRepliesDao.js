523a8b6061241f8172405ba8f31f73978ca08ba2
develckm <develckm@gmail.com>
2023-03-08 17:52:27 +0900

    BoardRepliesDao 완성 및 test 완료 insertOne 의 리턴을 등록된 id 로 함

---
+++ model/dao/BoardRepliesDao.js
@@ -0,0 +1,108 @@
+//BoardRepliesService
+//1.댓글 관리 리스트 [status 로 소메뉴, 페이징, 검색] //==board.findAll,findByStatus
+//2.댓글 관리 상태 업데이트
+//BoardService
+//3.게시글의 댓글 리스트를 출력 (게시글 상세)
+//4.게시글에서 댓글 작성(대댓글 작성)
+//5.게시글에서 댓글 삭제
+//6.게시글에서 댓글 수정
+class BoardRepliesDao{
+    #findAllSql="SELECT * FROM board_replies ORDER BY post_time DESC LIMIT ?,?";
+    #findByStatusSql="SELECT * FROM board_replies WHERE status=? ORDER BY post_time DESC LIMIT ?,?"; //PRIVATE(y),PUBLIC,REPORT(r),BLOCK(b)
+    #findByBidSql="SELECT * FROM board_replies WHERE b_id=? ORDER BY post_time DESC LIMIT ?,?"; //보드에 작성된 댓글(유저에게는 status="PUBLIC",로그인한 유저는 PRIVATE 까지 보여야함)
+    #insertOneSql="INSERT INTO board_replies (b_id, u_id, parent_br_id, img_path, content, status) VALUE (?,?,?,?,?,?)";
+    #updateByIdSql="UPDATE board_replies SET img_path=?,content=?,status=? WHERE br_id=?";
+    #updateStatusByIdSql="UPDATE board_replies SET status=? WHERE br_id=?";
+    #deleteByIdSql="DELETE FROM board_replies WHERE br_id=?";
+    #pageLength=10;
+    #pool;
+    constructor(pool) {
+        this.#pool=pool;
+    }
+    async findByAll(page=1){
+        const [rows,f]=await this.#pool.query(this.#findAllSql,[(page-1)*this.#pageLength,this.#pageLength]);
+        return rows;
+    }
+    async findByStatus(status,page=1){
+        const [rows,f]=await this.#pool.query(this.#findByStatusSql,[status,(page-1)*this.#pageLength,this.#pageLength]);
+        return rows;
+    }
+    async findByBid(bId,page=1){
+        const [rows,f]=await this.#pool.query(this.#findByBidSql,[bId,(page-1)*this.#pageLength,this.#pageLength]);
+        return rows;
+    }
+    async insertOne(reply){
+    /*reply= {
+            b_id: 7,
+            u_id: 'user06',
+            parent_br_id: null || br_id,
+            status: 'REPORT',
+            img_path: null,
+            content: '댓글입니다10'
+    }*/
+        //(b_id, u_id, parent_br_id, img_path, content, status);
+        const values=[reply.b_id,reply.u_id,reply.parent_br_id,reply.img_path,reply.content,reply.status];
+        let insertId=0;
+        try {
+            const [result]=await this.#pool.execute(this.#insertOneSql,values);
+            insertId=result.insertId;
+        /*
+        *   ResultSetHeader {
+              affectedRows: 1, //실행된 로우
+              insertId: 44,  //등록할 때 auto increment 로 생성된 pk
+            }
+        * */
+        }catch (e) {
+            throw new Error(e);
+        }
+        return insertId;
+    }
+    async updateById(reply){
+        let update=0;
+        /*reply= {
+            br_id: 7,
+            status: 'REPORT',
+            img_path: null,
+            content: '댓글입니다10'
+        }*/
+        //img_path=?,content=?,status=? WHERE br_id=?
+        try {
+            const values=[reply.img_path,reply.content,reply.status,reply.br_id];
+            const [result]=await this.#pool.execute(this.#updateByIdSql,values);
+            update=result.affectedRows;
+        }catch (e) {
+            throw new Error(e);
+        }
+        return update;
+    }
+    async updateStatusById(reply){
+        let update=0;
+        /*reply= {
+            br_id: 7,
+            status: 'REPORT',
+        }*/
+        //img_path=?,content=?,status=? WHERE br_id=?
+        try {
+            const values=[reply.status,reply.br_id];
+            const [result]=await this.#pool.execute(this.#updateStatusByIdSql,values);
+            update=result.affectedRows;
+        }catch (e) {
+            throw new Error(e);
+        }
+        return update;
+    }
+    async deleteById(br_id){
+        let del=0;
+        try {
+            const [result]=await this.#pool.execute(this.#deleteByIdSql,[br_id]);
+            del=result.affectedRows;
+        }catch (e) {
+            throw new Error(e);
+        }
+        return del;
+    }
+}
+
+module.exports=BoardRepliesDao;
+
+
---
+++ test/model/dao/BoardRepliesDao.test.js
@@ -0,0 +1,96 @@
+const pool=require("../../../model/db/WebAppBoardPool");
+const BoardRepliesDao=require("../../../model/dao/BoardRepliesDao");
+const boardRepliesDao=new BoardRepliesDao(pool);
+
+describe("BoardRepliesDao test",()=>{
+    let insertId=0; //삭제 수정 테스트에 사용할 등록된 번호
+
+    beforeAll(async ()=>{
+        const reply= {
+            b_id: 6,
+            u_id: 'user06',
+            parent_br_id: null,
+            status: 'REPORT',
+            img_path: "img/replies/1.jpeg",
+            content: '댓글 추가 테스트'
+        }
+        try {
+            insertId=await boardRepliesDao.insertOne(reply);
+        }catch (e) {
+            console.error(e);
+        }
+    });
+    test("findAll",async ()=>{
+        const rows=await boardRepliesDao.findByAll(1);
+        console.log(rows);
+    });
+    test("findByStatus",async ()=>{
+        const rows=await boardRepliesDao.findByStatus("PUBLIC",1);
+        console.log(rows);
+    });
+    test("findByBid",async ()=>{
+        const rows=await boardRepliesDao.findByBid(7,1);
+        console.log(rows);
+    });
+    test("insert",async ()=>{
+        const reply= {
+           b_id: 6,
+           u_id: 'user06',
+           parent_br_id: null,
+           status: 'REPORT',
+           img_path: "img/replies/1.jpeg",
+           content: '댓글 추가 테스트'
+        }
+        let insertId=0;
+        try {
+            insertId=await boardRepliesDao.insertOne(reply);
+        }catch (e) {
+            console.error(e);
+        }
+        expect(insertId).toBeGreaterThan(0);
+    });
+    test("updateById",async ()=>{
+        const reply= {
+            br_id: insertId,
+            status: 'REPORT',
+            img_path: "img/replies/1.jpeg",
+            content: '댓글 수정 테스트'
+        }
+        let update=0;
+        try {
+            update=await boardRepliesDao.updateById(reply);
+        }catch (e) {
+            console.error(e);
+        }
+        expect(update).toBeGreaterThan(0);
+    });
+    test("updateStatusById",async ()=>{
+        const reply= {
+            br_id: 16,
+            status: 'BLOCK',
+        }
+        let update=0;
+        try {
+            update=await boardRepliesDao.updateStatusById(reply);
+        }catch (e) {
+            console.error(e);
+        }
+        expect(update).toBeGreaterThan(0);
+    });
+    test("deleteById",async ()=>{
+        let del=0;
+        try{
+            del=await boardRepliesDao.deleteById(insertId);
+        }catch (e) {
+            console.error(e);
+        }
+        expect(del).toBeGreaterThan(0);
+
+    })
+});
+// 플젝!! 팀원 내일짜서 16일 17일
+// 프로젝트가 1 관리자(express+sequlize) 2 서비스(spring)
+// 다음주   목16 금17
+// 다다음주 월20 화21 =>2~4 (모두 나와야함)  수 목 금 4~5
+// 오후 4시 30분에 병합
+// 오후 5시  git push 검사
